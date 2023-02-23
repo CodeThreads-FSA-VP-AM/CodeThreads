10a6e91a734dbb92b97cac5a00de5fe08e42aa0a
AMT98 <aswinmalla12@gmail.com>
2023-02-23 14:07:12 -0700

    Change files to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
-import './style/index.css';
+import "./style/index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
