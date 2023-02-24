4ccae38e9ba5080c966703974f787907acaab755
AMT98 <aswinmalla12@gmail.com>
2023-02-23 22:25:15 -0700

    Navbar shows in the UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
 // import { getAPIHealth } from "../axios-services";
 import "../style/App.css";
+import Navbar from "./Navbar";
 
 const App: React.FC = () => {
   const [APIHealth, setAPIHealth] = useState("");
@@ -21,12 +23,16 @@ const App: React.FC = () => {
   //   // invoke it immediately after its declaration, inside the useEffect callback
   //   getAPIStatus();
   // }, []);
+  {
+    /* <p>API Status: {APIHealth}</p> */
+  }
 
   return (
-    <div className="app-container">
-      <h1>Hello, World!</h1>
-      {/* <p>API Status: {APIHealth}</p> */}
-    </div>
+    <>
+      <Router>
+        <Navbar />
+      </Router>
+    </>
   );
 };
 
