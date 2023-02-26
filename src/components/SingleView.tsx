72b09a1a4b22de280f8b7ff4d67337c51a73d1ba
vincent <vincentpalomo@yahoo.com>
2023-02-25 18:02:22 -0600

    added ui to products and singleview

--- src/components/SingleView.tsx
+++ src/components/SingleView.tsx
@@ -26,12 +26,171 @@ const SingleView: FC<Props> = ({ productId }) => {
 
   return (
     <>
-      <div>SingleView</div>
-      {productId}
-      {product?.title}
-      {product?.description}
-      {product?.price}
-      {product?.id}
+      <section>
+        <div className="relative max-w-screen-xl px-4 py-8 mx-auto">
+          <div className="grid items-start grid-cols-1 gap-8 md:grid-cols-2">
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-1">
+              <img
+                alt="Tee"
+                src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                className="h-72 w-full rounded-xl object-cover lg:h-[540px]"
+              />
+
+              {/* <div className="grid grid-cols-2 gap-4 lg:mt-4">
+                <img
+                  alt="Tee"
+                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  className="h-72 w-full rounded-xl object-cover lg:h-[540px]"
+                />
+              </div> */}
+            </div>
+
+            <div className="sticky top-0">
+              <strong className="rounded-full border border-blue-600 bg-gray-100 px-3 py-0.5 text-xs font-medium tracking-wide text-blue-600">
+                Pre Order
+              </strong>
+
+              <div className="flex justify-between mt-8">
+                <div className="max-w-[35ch] space-y-2">
+                  <h1 className="text-xl font-bold sm:text-2xl">{product?.title}</h1>
+
+                  <p className="text-sm">Highest Rated Product</p>
+
+                  <div className="-ml-0.5 flex">
+                    <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+
+                    <svg className="w-5 h-5 text-gray-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </div>
+                </div>
+
+                <p className="text-lg font-bold">£{product?.price} GBP</p>
+              </div>
+
+              <div className="mt-4">
+                <div className="prose max-w-none">
+                  <p>{product?.description}</p>
+                </div>
+
+                {/* <button className="mt-2 text-sm font-medium underline">Read More</button> */}
+              </div>
+
+              <form className="mt-8">
+                <fieldset>
+                  <legend className="mb-1 text-sm font-medium">Color</legend>
+
+                  <div className="flex flex-wrap gap-1">
+                    <label htmlFor="color_tt" className="cursor-pointer">
+                      <input type="radio" name="color" id="color_tt" className="sr-only peer" />
+
+                      <span className="inline-block px-3 py-1 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        Texas Tea
+                      </span>
+                    </label>
+
+                    <label htmlFor="color_fr" className="cursor-pointer">
+                      <input type="radio" name="color" id="color_fr" className="sr-only peer" />
+
+                      <span className="inline-block px-3 py-1 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        Fiesta Red
+                      </span>
+                    </label>
+
+                    <label htmlFor="color_cb" className="cursor-pointer">
+                      <input type="radio" name="color" id="color_cb" className="sr-only peer" />
+
+                      <span className="inline-block px-3 py-1 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        Cobalt Blue
+                      </span>
+                    </label>
+                  </div>
+                </fieldset>
+
+                <fieldset className="mt-4">
+                  <legend className="mb-1 text-sm font-medium">Size</legend>
+
+                  <div className="flex flex-wrap gap-1">
+                    <label htmlFor="size_xs" className="cursor-pointer">
+                      <input type="radio" name="size" id="size_xs" className="sr-only peer" />
+
+                      <span className="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        XS
+                      </span>
+                    </label>
+
+                    <label htmlFor="size_s" className="cursor-pointer">
+                      <input type="radio" name="size" id="size_s" className="sr-only peer" />
+
+                      <span className="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        S
+                      </span>
+                    </label>
+
+                    <label htmlFor="size_m" className="cursor-pointer">
+                      <input type="radio" name="size" id="size_m" className="sr-only peer" />
+
+                      <span className="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        M
+                      </span>
+                    </label>
+
+                    <label htmlFor="size_l" className="cursor-pointer">
+                      <input type="radio" name="size" id="size_l" className="sr-only peer" />
+
+                      <span className="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        L
+                      </span>
+                    </label>
+
+                    <label htmlFor="size_xl" className="cursor-pointer">
+                      <input type="radio" name="size" id="size_xl" className="sr-only peer" />
+
+                      <span className="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white">
+                        XL
+                      </span>
+                    </label>
+                  </div>
+                </fieldset>
+
+                <div className="flex gap-4 mt-8">
+                  <div>
+                    <label htmlFor="quantity" className="sr-only">
+                      Qty
+                    </label>
+
+                    <input
+                      type="number"
+                      id="quantity"
+                      min="1"
+                      value="1"
+                      className="w-12 rounded border-gray-200 py-3 text-center text-xs [-moz-appearance:_textfield] [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none"
+                    />
+                  </div>
+                  {/* remove disabled to use */}
+                  <button disabled type="submit" className="block px-5 py-3 text-xs font-medium text-white bg-green-600 rounded hover:bg-green-500">
+                    Add to Cart
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
