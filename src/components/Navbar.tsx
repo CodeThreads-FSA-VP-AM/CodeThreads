3cdfb6639a5e70c67972350deced61a267dc168a
vincent <vincentpalomo@yahoo.com>
2023-03-05 13:56:54 -0600

    updated black box around links on navbar, better visiability

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -87,21 +87,21 @@ const Navbar: React.FC<Props> = ({ user, token, setToken }) => {
                 <li>
                   <NavLink
                     to="/featured"
-                    className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline">
+                    className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-800 hover:underline">
                     Home
                   </NavLink>
                 </li>
                 <li>
                   <NavLink
                     to="/products"
-                    className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline">
+                    className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-800 hover:underline">
                     Mens
                   </NavLink>
                 </li>
                 <li>
                   <NavLink
                     to="/products"
-                    className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline">
+                    className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-800 hover:underline">
                     Womens
                   </NavLink>
                 </li>
@@ -110,14 +110,14 @@ const Navbar: React.FC<Props> = ({ user, token, setToken }) => {
                     <li>
                       <NavLink
                         to="./login"
-                        className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline">
+                        className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-800 hover:underline">
                         Login
                       </NavLink>
                     </li>
                     <li>
                       <NavLink
                         to="./register"
-                        className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800 hover:underline">
+                        className="text-base text-gray-800 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-800 hover:underline">
                         Sign Up
                       </NavLink>
                     </li>
@@ -138,7 +138,7 @@ const Navbar: React.FC<Props> = ({ user, token, setToken }) => {
                   <button
                     onClick={() => setSearchInput(!searchInput)}
                     aria-label="search items"
-                    className="text-gray-800 dark:hover:text-gray-300 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800">
+                    className="text-gray-800 dark:hover:text-gray-300 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800">
                     <svg className="fill-stroke" width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         d="M5 11C5 15.4183 8.58172 19 13 19C17.4183 19 21 15.4183 21 11C21 6.58172 17.4183 3 13 3C8.58172 3 5 6.58172 5 11Z"
@@ -168,7 +168,7 @@ const Navbar: React.FC<Props> = ({ user, token, setToken }) => {
                 <div className="items-center hidden space-x-4 lg:flex xl:space-x-8">
                   <button
                     aria-label="view favourites"
-                    className="text-gray-800 dark:hover:text-gray-900 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800">
+                    className="text-gray-800 dark:hover:text-gray-900 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-800">
                     <svg className="fill-stroke" width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         d="M20.8401 4.60987C20.3294 4.09888 19.7229 3.69352 19.0555 3.41696C18.388 3.14039 17.6726 2.99805 16.9501 2.99805C16.2276 2.99805 15.5122 3.14039 14.8448 3.41696C14.1773 3.69352 13.5709 4.09888 13.0601 4.60987L12.0001 5.66987L10.9401 4.60987C9.90843 3.57818 8.50915 2.99858 7.05012 2.99858C5.59109 2.99858 4.19181 3.57818 3.16012 4.60987C2.12843 5.64156 1.54883 7.04084 1.54883 8.49987C1.54883 9.95891 2.12843 11.3582 3.16012 12.3899L4.22012 13.4499L12.0001 21.2299L19.7801 13.4499L20.8401 12.3899C21.3511 11.8791 21.7565 11.2727 22.033 10.6052C22.3096 9.93777 22.4519 9.22236 22.4519 8.49987C22.4519 7.77738 22.3096 7.06198 22.033 6.39452C21.7565 5.72706 21.3511 5.12063 20.8401 4.60987V4.60987Z"
@@ -181,7 +181,7 @@ const Navbar: React.FC<Props> = ({ user, token, setToken }) => {
                   </button>
                   <button
                     aria-label="go to cart"
-                    className="text-gray-800 dark:hover:text-gray-300 dark:text-black focus:outline-none focus:ring-2 focus:ring-gray-800">
+                    className="text-gray-800 dark:hover:text-gray-300 dark:text-black focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-gray-800">
                     <NavLink to="/orders">
                       <svg className="fill-stroke" width={26} height={26} viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
