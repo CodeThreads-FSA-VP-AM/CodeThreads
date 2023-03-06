93f1f4c9804c124f4087397880deeee131562f2f
AMT98 <aswinmalla12@gmail.com>
2023-03-06 16:38:47 -0700

    Add loader to remaining components

--- src/components/AddReview.tsx
+++ src/components/AddReview.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { createReview } from "../api/api";
 import { Review } from "./Interfaces";
+import Loader from "./Loader";
 interface AddReviewProps {
   token: string;
   product_id: number;
@@ -12,8 +13,11 @@ const AddReview = (props: AddReviewProps) => {
   const [title, setTitle] = useState("");
   const [rating, setRating] = useState(0);
   const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState<Boolean>(true);
 
-  const handleCreateReview: React.FormEventHandler<HTMLFormElement> = async (e) => {
+  const handleCreateReview: React.FormEventHandler<HTMLFormElement> = async (
+    e
+  ) => {
     e.preventDefault();
     console.log(title, description, "rating", rating);
     try {
@@ -30,6 +34,7 @@ const AddReview = (props: AddReviewProps) => {
       setDescription("");
       props.setReviews([...props.reviews, newReview]);
       console.log(newReview);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -48,108 +53,158 @@ const AddReview = (props: AddReviewProps) => {
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
               <div className="-ml-0.5 flex">
-                <h3 className="mx-8 text-base font-semibold leading-6 text-gray-900">Review</h3>
-                <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <h3 className="mx-8 text-base font-semibold leading-6 text-gray-900">
+                  Review
+                </h3>
+                <svg
+                  className="w-5 h-5 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
 
-                <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <svg
+                  className="w-5 h-5 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
 
-                <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <svg
+                  className="w-5 h-5 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
 
-                <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <svg
+                  className="w-5 h-5 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
 
-                <svg className="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <svg
+                  className="w-5 h-5 text-yellow-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
               </div>
-              <p className="mx-8 text-sm text-gray-600">Please fill out all sections to add a review.</p>
+              <p className="mx-8 text-sm text-gray-600">
+                Please fill out all sections to add a review.
+              </p>
             </div>
           </div>
           <div className="mt-5 md:col-span-2 md:mt-0">
-            <form onSubmit={handleCreateReview}>
-              <div className="overflow-hidden shadow sm:rounded-md">
-                <div className="px-4 py-5 bg-white sm:p-6">
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-4">
-                      <label htmlFor="first-name" className="block text-sm font-medium text-gray-700" aria-required>
-                        Username<span className="text-[#F70000]">*</span>
-                      </label>
-                      <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        disabled
-                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
+            {loading ? (
+              <Loader />
+            ) : (
+              <form onSubmit={handleCreateReview}>
+                <div className="overflow-hidden shadow sm:rounded-md">
+                  <div className="px-4 py-5 bg-white sm:p-6">
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="col-span-6 sm:col-span-4">
+                        <label
+                          htmlFor="first-name"
+                          className="block text-sm font-medium text-gray-700"
+                          aria-required
+                        >
+                          Username<span className="text-[#F70000]">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="first-name"
+                          id="first-name"
+                          autoComplete="given-name"
+                          disabled
+                          className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="title" className="block text-sm font-medium text-gray-700" aria-required="true">
-                        Title<span className="text-[#F70000]">*</span>
-                      </label>
-                      <input
-                        type="text"
-                        name="title"
-                        id="title"
-                        autoComplete="title"
-                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                      />
-                    </div>
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="title"
+                          className="block text-sm font-medium text-gray-700"
+                          aria-required="true"
+                        >
+                          Title<span className="text-[#F70000]">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="title"
+                          id="title"
+                          autoComplete="title"
+                          className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                          value={title}
+                          onChange={(e) => setTitle(e.target.value)}
+                        />
+                      </div>
 
-                    <div className="col-span-6 sm:col-span-1">
-                      <label htmlFor="country" className="block text-sm font-medium text-gray-700" aria-required="true">
-                        Rating<span className="text-[#F70000]">*</span>
-                      </label>
-                      <select
-                        id="country"
-                        name="country"
-                        autoComplete="country-name"
-                        className="block w-full px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                        value={rating}
-                        defaultValue={1}
-                        onChange={(e) => setRating(parseInt(e.target.value))}>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </select>
-                    </div>
+                      <div className="col-span-6 sm:col-span-1">
+                        <label
+                          htmlFor="country"
+                          className="block text-sm font-medium text-gray-700"
+                          aria-required="true"
+                        >
+                          Rating<span className="text-[#F70000]">*</span>
+                        </label>
+                        <select
+                          id="country"
+                          name="country"
+                          autoComplete="country-name"
+                          className="block w-full px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                          value={rating}
+                          defaultValue={1}
+                          onChange={(e) => setRating(parseInt(e.target.value))}
+                        >
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                        </select>
+                      </div>
 
-                    <div className="col-span-4">
-                      <label htmlFor="description" className="block text-sm font-medium text-gray-700">
-                        Description
-                      </label>
-                      <textarea
-                        name="description"
-                        id="description"
-                        autoComplete="description"
-                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                      />
+                      <div className="col-span-4">
+                        <label
+                          htmlFor="description"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Description
+                        </label>
+                        <textarea
+                          name="description"
+                          id="description"
+                          autoComplete="description"
+                          className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                          value={description}
+                          onChange={(e) => setDescription(e.target.value)}
+                        />
+                      </div>
                     </div>
                   </div>
+                  <div className="px-4 py-3 text-left bg-gray-50 sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      Create
+                    </button>
+                  </div>
                 </div>
-                <div className="px-4 py-3 text-left bg-gray-50 sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    Create
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
+            )}
           </div>
         </div>
       </div>
