ffcfcb572e77f2e2c159476c3e3498377fac9790
AMT98 <aswinmalla12@gmail.com>
2023-03-12 23:36:27 -0600

    Add required to forms

--- src/components/AccountSettings.tsx
+++ src/components/AccountSettings.tsx
@@ -68,13 +68,14 @@ const AccountSettings: React.FC<Props> = (props) => {
                         htmlFor="first-name"
                         className="block text-sm font-medium leading-6 text-gray-900"
                       >
-                        New username
+                        New username<span className="text-[#F70000]">*</span>
                       </label>
                       <input
                         type="text"
                         name="first-name"
                         id="first-name"
                         autoComplete="given-name"
+                        required
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                         className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -85,13 +86,15 @@ const AccountSettings: React.FC<Props> = (props) => {
                         htmlFor="last-name"
                         className="block text-sm font-medium leading-6 text-gray-900"
                       >
-                        New password
+                        New password<span className="text-[#F70000]">*</span>
                       </label>
                       <input
                         type="password"
                         name="last-name"
                         id="last-name"
                         autoComplete="given-name"
+                        required
+                        minLength={8}
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -104,12 +107,14 @@ const AccountSettings: React.FC<Props> = (props) => {
                         className="block text-sm font-medium leading-6 text-gray-900"
                       >
                         New email address
+                        <span className="text-[#F70000]">*</span>
                       </label>
                       <input
                         type="email"
                         name="email-address"
                         id="email-address"
                         autoComplete="email"
+                        required
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
