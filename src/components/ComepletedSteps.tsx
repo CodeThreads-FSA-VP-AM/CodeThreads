cd83406f1c38e53c99afe1f4db0bcd720fcd4a2c
AMT98 <aswinmalla12@gmail.com>
2023-03-13 23:39:03 -0600

    Created completed steps

---
+++ src/components/ComepletedSteps.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+type Props = {};
+
+const Steps = (props: Props) => {
+  return (
+    <div className="pt-6">
+      <h2 className="sr-only">Steps</h2>
+
+      <div className="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-400">
+        <ol className="relative z-10 flex justify-between text-sm font-medium text-gray-500">
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-green-500 text-center text-[10px] font-bold leading-6 text-white">
+              1
+            </span>
+
+            <span className="hidden sm:block"> Added </span>
+          </li>
+
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-green-500 text-center text-[10px] font-bold leading-6 text-white">
+              2
+            </span>
+
+            <span className="hidden sm:block"> Processing </span>
+          </li>
+
+          <li className="flex items-center gap-2 bg-white p-2">
+            <span className="h-6 w-6 rounded-full bg-green-500 text-center text-[10px] font-bold leading-6 text-white">
+              3
+            </span>
+
+            <span className="hidden sm:block"> Completed </span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Steps;
