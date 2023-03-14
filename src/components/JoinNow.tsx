8d64d04b991a78c231acb43e3fbd791497ca12e8
AMT98 <aswinmalla12@gmail.com>
2023-03-14 01:12:50 -0600

    Changed styling of the joinnow banner

--- src/components/JoinNow.tsx
+++ src/components/JoinNow.tsx
@@ -5,10 +5,10 @@ type Props = {};
 
 const JoinNow = (props: Props) => {
   return (
-    <div className="container mx-auto py-9 md:py-12 px-4 md:px-6 text-white border rounded-xl shadow-2xl">
+    <div className="container mx-auto py-9 md:py-12 px-4 md:px-6 text-black ">
       <NavLink to="/register">
         <div className="flex items-strech justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8 border rounded-xl shadow-2xl">
-          <div className="flex flex-col md:flex-row items-strech justify-between bg-black py-6 px-6 md:py-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-[100%] border rounded-xl shadow-2xl">
+          <div className="flex flex-col md:flex-row items-strech justify-between bg-gray-300 py-6 px-6 md:py-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-[100%] border rounded-md shadow-md">
             <div className="flex flex-col justify-center md:w-1/2">
               <h1 className="text-3xl lg:text-4xl font-semibold">
                 Become a member
@@ -30,7 +30,7 @@ const JoinNow = (props: Props) => {
               </div>
             </div>
             <div className="md:w-[22%] mt-8 md:mt-0 flex items-center justify-center md:justify-end ">
-              <p className="text-6xl font-bold">codeThreads</p>
+              <p className="text-6xl font-bold text-gray-900">codeThreads</p>
             </div>
           </div>
         </div>
