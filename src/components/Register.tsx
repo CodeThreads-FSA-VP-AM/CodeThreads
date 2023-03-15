121bcecfb06ca71d88072eac232c6356bbda2cb5
AMT98 <aswinmalla12@gmail.com>
2023-03-14 21:49:13 -0600

    Added some parts of google OAuth

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,7 +1,7 @@
 import React, { useState, FormEvent } from "react";
 import { fetchRegister } from "../api/api";
 import Loader from "./Loader";
-
+import jwt_decode from "jwt-decode";
 // type Props = {};
 
 type Register = {
@@ -17,6 +17,7 @@ const Register = () => {
   const [errorMsg, setErrorMsg] = useState("");
   const [sidebar, setsidebar] = useState();
   const [loading, setLoading] = useState<Boolean>(false);
+  const [user, setUser] = useState({});
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -40,6 +41,27 @@ const Register = () => {
       setLoading(false);
     }
   };
+  function handleCredentialResponse(response: any) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    try {
+      const userObject = jwt_decode(response.credential);
+      console.log(userObject);
+      if (userObject) {
+        setUser(userObject);
+      }
+    } catch (error) {
+      console.error("Error decoding JWT:", error);
+    }
+  }
+  window.onload = function () {
+    google.accounts.id.initialize({
+      client_id:
+        "137794005516-kiiplu4qkptolsv7oga14rts43ecjfbb.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+    });
+
+    google.accounts.id.prompt(); // also display the One Tap dialog
+  };
 
   return (
     <>
@@ -58,39 +80,23 @@ const Register = () => {
                 Sign Up
               </p>
 
-              <button
-                aria-label="Continue with google"
-                role="button"
-                className="focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-700 py-3.5 px-4 border rounded-lg border-gray-700 flex items-center w-full mt-10"
-              >
-                <svg
-                  width={19}
-                  height={20}
-                  viewBox="0 0 19 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M18.9892 10.1871C18.9892 9.36767 18.9246 8.76973 18.7847 8.14966H9.68848V11.848H15.0277C14.9201 12.767 14.3388 14.1512 13.047 15.0812L13.0289 15.205L15.905 17.4969L16.1042 17.5173C17.9342 15.7789 18.9892 13.221 18.9892 10.1871Z"
-                    fill="#4285F4"
-                  />
-                  <path
-                    d="M9.68813 19.9314C12.3039 19.9314 14.4999 19.0455 16.1039 17.5174L13.0467 15.0813C12.2286 15.6682 11.1306 16.0779 9.68813 16.0779C7.12612 16.0779 4.95165 14.3395 4.17651 11.9366L4.06289 11.9465L1.07231 14.3273L1.0332 14.4391C2.62638 17.6946 5.89889 19.9314 9.68813 19.9314Z"
-                    fill="#34A853"
-                  />
-                  <path
-                    d="M4.17667 11.9366C3.97215 11.3165 3.85378 10.6521 3.85378 9.96562C3.85378 9.27905 3.97215 8.6147 4.16591 7.99463L4.1605 7.86257L1.13246 5.44363L1.03339 5.49211C0.37677 6.84302 0 8.36005 0 9.96562C0 11.5712 0.37677 13.0881 1.03339 14.4391L4.17667 11.9366Z"
-                    fill="#FBBC05"
-                  />
-                  <path
-                    d="M9.68807 3.85336C11.5073 3.85336 12.7344 4.66168 13.4342 5.33718L16.1684 2.59107C14.4892 0.985496 12.3039 0 9.68807 0C5.89885 0 2.62637 2.23672 1.0332 5.49214L4.16573 7.99466C4.95162 5.59183 7.12608 3.85336 9.68807 3.85336Z"
-                    fill="#EB4335"
-                  />
-                </svg>
-                <p className="ml-4 text-base font-medium text-gray-700">
-                  Continue with Google
-                </p>
-              </button>
+              <div
+                id="g_id_onload"
+                data-client_id="137794005516-kiiplu4qkptolsv7oga14rts43ecjfbb.apps.googleusercontent.com"
+                data-context="signup"
+                data-ux_mode="popup"
+                data-login_uri="http://localhost:3000"
+                data-auto_prompt="false"
+              ></div>
+              <div
+                className="g_id_signin"
+                data-type="standard"
+                data-shape="rectangular"
+                data-theme="outline"
+                data-text="signin_with"
+                data-size="large"
+                data-logo_alignment="left"
+              ></div>
               <button
                 aria-label="Continue with github"
                 role="button"
