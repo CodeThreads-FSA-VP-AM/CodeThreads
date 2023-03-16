a856a9fc96a08179034d31cc59c5bd111d9bf2ff
Vincent Palomo <vincentpalomo@yahoo.com>
2023-03-15 20:39:54 -0500

    able to render the set amount of products per page, pagination rendered on the botton but not working

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -15,8 +15,33 @@ const Products: React.FC<Props> = ({ setProductId, user }) => {
   const [selectedId, setSelectedId] = useState(0);
   const [message, setMessage] = useState('');
   const [search, setSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
-  console.log(user.is_admin);
+  const productsPerPage = 8; // set how many products to display on the page
+
+  // find out how to get the index of the products in the currentpage
+  // how to get the index of the previous page  x
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+
+  const filteredProducts = products.filter((p) => {
+    if (search === '') {
+      return p;
+    } else if (p.title.toLowerCase().includes(search)) {
+      return p.title;
+    } else if (p.tags.some((t: any) => t.name.toLowerCase() === search)) {
+      return p;
+    }
+  });
+
+  // filter the products to only show the set products perpage
+  const totalProducts = filteredProducts.length;
+  const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+  // page handler to changing the page of products
+  const handlePageClick = (page: number) => {
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
     const loadProducts = async () => {
@@ -120,135 +145,78 @@ const Products: React.FC<Props> = ({ setProductId, user }) => {
               // </div>
               <Loader />
             ) : (
-              products
-                .filter((p) => {
-                  if (search === '') {
-                    return p;
-                  } else if (p.title.toLowerCase().includes(search)) {
-                    return p.title;
-                  } else if (p.tags.some((t: any) => t.name.toLowerCase() === search)) {
-                    return p;
-                  }
-                })
-                ?.map((p: Product) => (
-                  <div key={p.id}>
-                    <Link
-                      to={`/products/${p.id}`}
-                      className='relative block mb-2 overflow-hidden bg-gray-100 rounded-lg shadow-lg group h-96 lg:mb-3'
-                      onClick={() => idHandle(p.id)}
-                    >
-                      <img
-                        src={p.front_url}
-                        loading='lazy'
-                        alt=''
-                        className='object-cover object-center w-full h-full transition duration-200 group-hover:scale-110'
-                      />
+              filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct)?.map((p: Product) => (
+                <div key={p.id}>
+                  <Link
+                    to={`/products/${p.id}`}
+                    className='relative block mb-2 overflow-hidden bg-gray-100 rounded-lg shadow-lg group h-96 lg:mb-3'
+                    onClick={() => idHandle(p.id)}
+                  >
+                    <img
+                      src={p.front_url}
+                      loading='lazy'
+                      alt=''
+                      className='object-cover object-center w-full h-full transition duration-200 group-hover:scale-110'
+                    />
 
-                      <div className='absolute flex gap-2 left-2 bottom-2'>
-                        {/* <span className="bg-red-500 text-white text-sm font-semibold tracking-wider uppercase rounded-r-lg px-3 py-1.5">-50%</span> */}
-                        {p.tags?.map((t: any) => {
-                          return (
-                            <span key={t.id} className='bg-white text-gray-800 text-sm font-bold tracking-wider uppercase rounded-lg px-3 py-1.5'>
-                              {t.name}
-                            </span>
-                          );
-                        })}
-                      </div>
-                    </Link>
+                    <div className='absolute flex gap-2 left-2 bottom-2'>
+                      {/* <span className="bg-red-500 text-white text-sm font-semibold tracking-wider uppercase rounded-r-lg px-3 py-1.5">-50%</span> */}
+                      {p.tags?.map((t: any) => {
+                        return (
+                          <span key={t.id} className='bg-white text-gray-800 text-sm font-bold tracking-wider uppercase rounded-lg px-3 py-1.5'>
+                            {t.name}
+                          </span>
+                        );
+                      })}
+                    </div>
+                  </Link>
 
-                    <div className='flex items-start justify-between gap-2 px-2'>
-                      <div className='flex flex-col'>
-                        <a href='#' className='text-lg font-bold text-gray-800 capitalize transition duration-100 hover:text-gray-500 lg:text-xl'>
-                          {p.title}
-                        </a>
-                        <span className='text-gray-500'>by codeThreads</span>
-                      </div>
+                  <div className='flex items-start justify-between gap-2 px-2'>
+                    <div className='flex flex-col'>
+                      <a href='#' className='text-lg font-bold text-gray-800 capitalize transition duration-100 hover:text-gray-500 lg:text-xl'>
+                        {p.title}
+                      </a>
+                      <span className='text-gray-500'>by codeThreads</span>
+                    </div>
 
-                      <div className='flex flex-col items-end'>
-                        <span className='font-bold text-gray-600 lg:text-lg'> £{p.price} GBP </span>
-                        {/* <span className="text-sm text-red-500 line-through">$39.99</span> */}
-                      </div>
+                    <div className='flex flex-col items-end'>
+                      <span className='font-bold text-gray-600 lg:text-lg'> £{p.price} GBP </span>
+                      {/* <span className="text-sm text-red-500 line-through">$39.99</span> */}
                     </div>
                   </div>
-                ))
+                </div>
+              ))
             )}
             {/* <!-- product - end --> */}
           </div>
+          {/* pagination */}
+          {totalProducts > 0 && (
+            <nav className='flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6' aria-label='Pagination'>
+              <div className='hidden sm:block'>
+                <p className='text-sm text-gray-700'>
+                  Showing <span className='font-medium'>1</span> to <span className='font-medium'>10</span> of <span className='font-medium'>20</span>{' '}
+                  results
+                </p>
+              </div>
+              <div className='flex flex-1 justify-between sm:justify-end'>
+                <a
+                  href='#'
+                  className='relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50'
+                >
+                  Previous
+                </a>
+                <a
+                  href='#'
+                  className='relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50'
+                >
+                  Next
+                </a>
+              </div>
+            </nav>
+          )}
         </div>
       </section>
     </>
-
-    // <>
-    //   <section>
-    //     <div classNameName="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
-    //       <header>
-    //         <h2 classNameName="text-xl font-bold text-gray-900 sm:text-3xl">Product Collection</h2>
-
-    //         <p classNameName="max-w-md mt-4 text-gray-500">
-    //           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque praesentium cumque iure dicta incidunt est ipsam, officia dolor fugit
-    //           natus?
-    //         </p>
-
-    //         {user.is_admin && (
-    //           <>
-    //             <Link to="/addproduct" classNameName="flex justify-center">
-    //               <button>add</button>
-    //             </Link>
-
-    //             <div>
-    //               <select onChange={handleSelect}>
-    //                 <option value="delete">delete product</option>
-    //                 {products.map((p) => (
-    //                   <option value={p.id} key={p.id}>
-    //                     {p.title}
-    //                   </option>
-    //                 ))}
-    //               </select>
-
-    //               <button classNameName="px-1" onClick={handleDelete}>
-    //                 delete
-    //               </button>
-    //               <span classNameName="text-red-400">{message}</span>
-    //             </div>
-    //           </>
-    //         )}
-    //       </header>
-
-    //       <ul classNameName="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4">
-    //         {/* map over this code */}
-
-    //         {loading ? (
-    //           // <div>Loading...</div>
-    //           <div classNameName="flex">
-    //             <div classNameName="flex items-center justify-center w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-blue-400"></div>
-    //           </div>
-    //         ) : (
-    //           products?.map((p: Product) => (
-    //             <li key={p.id}>
-    //               <Link to={`/products/${p.id}`} classNameName="block overflow-hidden group" onClick={() => idHandle(p.id)}>
-    //                 <img
-    //                   src={p.front_url}
-    //                   alt=""
-    //                   classNameName="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-    //                 />
-
-    //                 <div classNameName="relative pt-3 bg-white">
-    //                   <h3 classNameName="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">{p.title}</h3>
-
-    //                   <p classNameName="mt-2">
-    //                     <span classNameName="sr-only"> Regular Price </span>
-
-    //                     <span classNameName="tracking-wider text-gray-900"> £{p.price} GBP </span>
-    //                   </p>
-    //                 </div>
-    //               </Link>
-    //             </li>
-    //           ))
-    //         )}
-    //       </ul>
-    //     </div>
-    //   </section>
-    // </>
   );
 };
 
