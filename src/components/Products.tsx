a89a8b7798c2e98f45d6a9e3dda530bacf614ea5
Vincent Palomo <vincentpalomo@yahoo.com>
2023-03-15 21:16:15 -0500

    pagination is working, bug with going before and after the total number of pages

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -194,23 +194,30 @@ const Products: React.FC<Props> = ({ setProductId, user }) => {
             <nav className='flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6' aria-label='Pagination'>
               <div className='hidden sm:block'>
                 <p className='text-sm text-gray-700'>
-                  Showing <span className='font-medium'>1</span> to <span className='font-medium'>10</span> of <span className='font-medium'>20</span>{' '}
-                  results
+                  Showing <span className='font-medium'>1</span> to <span className='font-medium'> 10 </span> of{' '}
+                  <span className='font-medium'>20</span> products
                 </p>
               </div>
-              <div className='flex flex-1 justify-between sm:justify-end'>
-                <a
-                  href='#'
-                  className='relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50'
-                >
-                  Previous
-                </a>
-                <a
-                  href='#'
-                  className='relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50'
-                >
-                  Next
-                </a>
+              <div className='flex justify-center flex-1 sm:justify-end'>
+                <nav className='flex gap-2' aria-label='Pagination'>
+                  <button
+                    onClick={() => handlePageClick(currentPage - 1)}
+                    className={`px-3 py-1 rounded-md transition duration-150 ease-in-out ${
+                      currentPage === 1 ? 'text-gray-400' : 'text-gray-500 hover:bg-gray-200'
+                    }`}
+                  >
+                    Previous
+                  </button>
+
+                  <button
+                    onClick={() => handlePageClick(currentPage + 1)}
+                    className={`px-3 py-1 rounded-md transition duration-150 ease-in-out ${
+                      currentPage === totalPages ? 'text-gray-400' : 'text-gray-500 hover:bg-gray-200'
+                    }`}
+                  >
+                    Next
+                  </button>
+                </nav>
               </div>
             </nav>
           )}
