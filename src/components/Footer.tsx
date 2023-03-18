0d56b1fa18555c9a37f85ce836b3245e6ac7d0a1
Vincent Palomo <vincentpalomo@yahoo.com>
2023-03-18 14:39:15 -0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,80 +1,80 @@
-import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
+import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
 
 const Footer: React.FC = () => {
   return (
     <>
-      <div className="container z-50 grid items-center justify-center px-4 pt-16 pb-2 mx-auto xl:px-20 lg:px-12 sm:px-6">
-        <div className="grid items-center justify-center grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 md:gap-8">
-          <div className="flex flex-col">
-            <div>
-              <p className="text-xl font-bold">codeThreads</p>
-            </div>
-            <p className="mt-4 text-sm leading-none text-gray-800">Copyright © 2023 codeThreads</p>
-            <p className="mt-4 text-sm leading-none text-gray-800">All rights reserved</p>
+      <div className='container z-50 flex flex-col items-center justify-center px-4 pt-16 pb-2 mx-auto xl:px-20 lg:px-12 sm:px-6'>
+        <div className='flex flex-wrap gap-4 md:justify-start'>
+          <div className='flex flex-col'>
+            <p className='text-xl font-bold'>codeThreads</p>
+            <p className='mt-4 text-sm leading-none text-gray-800'>Copyright © 2023 codeThreads</p>
+            <p className='mt-4 text-sm leading-none text-gray-800'>All rights reserved</p>
           </div>
-          <div className="flex flex-row gap-2 md:gap-12">
-            <div className="">
-              <h2 className="text-base font-bold leading-4 text-gray-800">Vincent Palomo</h2>
+          <div className='flex flex-wrap gap-2 md:gap-12 justify-center items-center'>
+            <div>
+              <h2 className='text-base font-bold leading-4 text-gray-800'>Vincent Palomo</h2>
               <a
-                href="https://github.com/vincentpalomo"
-                target="_blank"
-                className="flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500">
+                href='https://github.com/vincentpalomo'
+                target='_blank'
+                className='flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500'
+              >
                 <FaGithub /> Github
               </a>
               <a
-                href="https://www.linkedin.com/in/vincent-palomo/"
-                target="_blank"
-                className="flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500">
-                <FaLinkedin color="blue" /> LinkedIn
+                href='https://www.linkedin.com/in/vincent-palomo/'
+                target='_blank'
+                className='flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500'
+              >
+                <FaLinkedin color='blue' /> LinkedIn
               </a>
               <a
-                href="mailto: vincentpalomo@yahoo.com"
-                target="_blank"
-                className="flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500">
-                <HiOutlineMail color="red" /> Mail
+                href='mailto: vincentpalomo@yahoo.com'
+                target='_blank'
+                className='flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500'
+              >
+                <HiOutlineMail color='red' /> Mail
               </a>
             </div>
             <div>
-              <h2 className="text-base font-bold leading-4 text-gray-800">Aswin Malla</h2>
+              <h2 className='text-base font-bold leading-4 text-gray-800'>Aswin Malla</h2>
               <a
-                href="https://github.com/amt98"
-                target="_blank"
-                className="flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500">
+                href='https://github.com/amt98'
+                target='_blank'
+                className='flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500'
+              >
                 <FaGithub /> Github
               </a>
               <a
-                href="https://www.linkedin.com/in/aswinmalla/"
-                target="_blank"
-                className="flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500">
-                <FaLinkedin color="blue" /> LinkedIn
+                href='https://www.linkedin.com/in/aswinmalla/'
+                target='_blank'
+                className='flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500'
+              >
+                <FaLinkedin color='blue' /> LinkedIn
               </a>
               <a
-                href="mailto: aswinmalla12@gmail.com"
-                target="_blank"
-                className="flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500">
-                <HiOutlineMail color="red" /> Mail
+                href='mailto: aswinmalla12@gmail.com'
+                target='_blank'
+                className='flex flex-row gap-2 mt-6 text-base leading-4 text-gray-800 cursor-pointer hover:text-gray-500'
+              >
+                <HiOutlineMail color='red' /> Mail
               </a>
             </div>
           </div>
         </div>
-        <div className="pt-6">
-          <ul className="flex flex-wrap justify-start gap-4 text-xs lg:justify-center">
+        <div className='pt-6'>
+          <ul className='flex flex-wrap justify-start gap-4 text-xs lg:justify-center'>
             <li>
-              <p className="text-gray-500 transition cursor-pointer hover:opacity-75">
-                Terms & Conditions
-              </p>
+              <p className='text-gray-500 transition cursor-pointer hover:opacity-75'>Terms & Conditions</p>
             </li>
 
             <li>
-              <p className="text-gray-500 transition cursor-pointer hover:opacity-75">
-                Privacy Policy
-              </p>
+              <p className='text-gray-500 transition cursor-pointer hover:opacity-75'>Privacy Policy</p>
             </li>
 
             <li>
-              <p className="text-gray-500 transition cursor-pointer hover:opacity-75">Cookies</p>
+              <p className='text-gray-500 transition cursor-pointer hover:opacity-75'>Cookies</p>
             </li>
           </ul>
         </div>
